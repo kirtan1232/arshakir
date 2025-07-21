dc4bf21dc86adcecb616935527160bf23e8e4209
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-07-21 15:20:26 +0545

    home

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to the Homepage</h1>
+      <p>This is the homepage of your application. Feel free to explore!</p>
+    </div>
+  );
+};
+
+export default HomePage;
